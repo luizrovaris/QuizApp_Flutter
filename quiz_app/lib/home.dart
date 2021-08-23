ac005518a4e187694903303e359c63b80a1379c1
luizrovaris <luiz_rovaris@hotmail.com>
2021-08-23 19:06:28 +0100

    Creating question box

--- quiz_app/lib/home.dart
+++ quiz_app/lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './models/question.dart';
+import './question_box.dart';
 
 class Home extends StatefulWidget{
   @override
@@ -6,16 +8,46 @@ class Home extends StatefulWidget{
 }
 
 class HomeState extends State<Home> {
-  final questions = [];
+  final _questions = [
+    {
+      0: [
+        Question('A seleção brasileira de futebol possui 5 títulos mundiais', 1),
+        Question('A seleção brasileira de futebol possui 6 títulos mundiais', 0)
+      ]
+    },
+    {
+      1: [
+        Question('O Flamengo é campeão mundial', 1),
+        Question('O Flamengo não é campeão mundial', 0)]
+    },
+    {
+      2: [
+        Question('O Palmeiras é campeão mundial', 0),
+        Question('O Palmeiras não é campeão mundial', 1)]
+    },
+    {
+      3: [
+        Question('O atual técnico da seleção brasileira é o Tite', 1),
+        Question('O atual técnico da seleção brasileira é o Luxa', 0)]
+    }
+  ];
   int result = 0;
-  
+  int _index = 0;
+
+  void nextQuestion(int value){
+    setState(() {
+      _index++;
+      result += value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Questions'),
       ),
-      body: Text('body')
+      body: QuestionBox(_index, _questions, nextQuestion)
     );
   }
 }
---
+++ quiz_app/lib/question_box.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class QuestionBox extends StatelessWidget{
+  final _index;
+  final _questions;
+  final Function nextQuestion;
+
+  QuestionBox(this._index, this._questions, this.nextQuestion);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+              Text('Seleciona a afirmação correta')
+            ]
+        ),
+        Row(
+          children: <Widget>[ElevatedButton(
+            onPressed: () => this.nextQuestion(_questions[_index][_index][0].value),
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+            ),
+            child: Text(_questions[_index][_index][0].question)
+          )],
+        ),
+        Row(
+          children: <Widget>[
+            Text('OU')
+          ],
+        ),
+        Row(
+          children: <Widget>[ElevatedButton(
+            onPressed: () => this.nextQuestion(_questions[_index][_index][1].value),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.green,
+              onPrimary: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25)
+              ),
+              elevation: 15.0
+            ),
+            child: Text(_questions[_index][_index][1].question)
+          )],
+        )
+      ],
+    );
+  }
+}
